e37958e7747ff7891e3a8fd0a4971232f1c85679
Irsyad1609 <irsyadvekok@gmail.com>
2026-01-01 22:42:21 +0100

    refactor build script to include architecture in ISO naming and improve checksum generation output

--- build.sh
+++ build.sh
@@ -24,13 +24,22 @@ docker run --privileged --rm \
     -v "$(pwd):/build" \
     $IMAGE_NAME
 
-# Rename ISO
 VERSION="1.0"
-ISO_NAME="FloofOS-${VERSION}.iso"
+ARCH="amd64"
+ISO_NAME="FloofOS-${VERSION}-${ARCH}.iso"
 
 if [ -f floofos-core-amd64.hybrid.iso ]; then
     mv floofos-core-amd64.hybrid.iso "$ISO_NAME"
-    echo "Build Complete: $ISO_NAME"
+    echo "Generating checksums..."
+    sha256sum "$ISO_NAME" > "${ISO_NAME}.sha256"
+    md5sum "$ISO_NAME" > "${ISO_NAME}.md5"
+    echo ""
+    echo "Build Complete!"
+    echo "  ISO:      $ISO_NAME"
+    echo "  Size:     $(du -h "$ISO_NAME" | cut -f1)"
+    echo "  SHA256:   $(cat "${ISO_NAME}.sha256" | cut -d' ' -f1)"
+    echo "  MD5:      $(cat "${ISO_NAME}.md5" | cut -d' ' -f1)"
+    echo ""
 else
     echo "Error: ISO not found!"
     exit 1
